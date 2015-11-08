3a119310e4e0070d2ce98831e767270d8af669a6
David Etler <davidretler@gmail.com>
2015-11-07 19:51:56 -0500

    Changed one thing

--- hipchat/main.js
+++ hipchat/main.js
@@ -1,6 +1,29 @@
+/*
 $(function() {
   $('.hipchat-ui').hipChatPanel({
     url: "https://www.hipchat.com/gU2LZswfh",
     timezone: "EST"
   });
 });
+*/
+
+window.location.replace("https://www.hipchat.com/gU2LZswfh");
+
+/*
+function getQueryParams(qs) {
+    qs = qs.split('+').join(' ');
+
+    var params = {},
+        tokens,
+        re = /[?&]?([^=]+)=([^&]*)/g;
+
+    while (tokens = re.exec(qs)) {
+        params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+    }
+
+    return params;
+}
+
+var query = getQueryParams(document.location.search);
+alert(query.room);
+*/
