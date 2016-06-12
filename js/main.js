ce6a8fe836ae3bdc709c14eb6b20e3615af513ff
David Etler <davidretler@gmail.com>
2016-06-12 13:03:03 -0400

    Tabs versus spaces

--- js/main.js
+++ js/main.js
@@ -1,47 +1,47 @@
 
 $(document).ready(function(){
-	
-		/*
-		* Set bindings for items in div.content
-		*/
-		function setContentBindings() {
-			//link to projects
-			$(".content a#projects").click(function(){
-				$("#my-navbar li a#projects").click();
-			});
-		}
+  
+    /*
+    * Set bindings for items in div.content
+    */
+    function setContentBindings() {
+      //link to projects
+      $(".content a#projects").click(function(){
+        $("#my-navbar li a#projects").click();
+      });
+    }
  
-		/*
-		* Fades the div out, loads new content, and fades back in
-		*/
-		function fadeAndLoad(content_url) {
-			
-			 
-			 $( ".content" ).fadeOut(300, function(){
-					 $( ".content" ).load(content_url, function(){
-							 $( ".content" ).fadeIn(300);
-							 setContentBindings();
-					 });
-			 });
-		}
+    /*
+    * Fades the div out, loads new content, and fades back in
+    */
+    function fadeAndLoad(content_url) {
+      
+       
+       $( ".content" ).fadeOut(300, function(){
+           $( ".content" ).load(content_url, function(){
+               $( ".content" ).fadeIn(300);
+               setContentBindings();
+           });
+       });
+    }
  
-		/*
-		* Sets the specified element as active, and deactivates all others
-		*/
-		function setActive(element)
-		{
-			//if the selected element isn't already active
-			if(!element.hasClass("active"))
-			{
-					$('.active').toggleClass("active"); //turn off all active elements
-					element.toggleClass("active");      //set the current element to active
-			}
-		}
+    /*
+    * Sets the specified element as active, and deactivates all others
+    */
+    function setActive(element)
+    {
+      //if the selected element isn't already active
+      if(!element.hasClass("active"))
+      {
+          $('.active').toggleClass("active"); //turn off all active elements
+          element.toggleClass("active");      //set the current element to active
+      }
+    }
  
-		/*
+    /*
      * Code to change content when menubar item is clicked (load internal content)
      */
-			$("#my-navbar li a.internal").click(function() {
+      $("#my-navbar li a.internal").click(function() {
         //if the element clicked is not an external link
         if($(this).attr("id") != [])
         {
@@ -60,8 +60,8 @@ $(document).ready(function(){
             }
         }
     });
-		
-		 /*
+    
+     /*
      * Open Resume in new window
      */
     $("#my-navbar li a#resume").click(function() {
@@ -74,27 +74,27 @@ $(document).ready(function(){
             window.location.href = 'content/Resume/david_etler.pdf';
         }
     });
-		
+    
     $(document).click(function (event) {
-			
+      
         var clickover = $(event.target);
         var _opened = $("#my-navbar li a#about").is(":visible") && $("button.navbar-toggle").is(":visible");
         if (_opened === true && !clickover.hasClass("navbar-toggle")) {
-						$("button.navbar-toggle").click();
+            $("button.navbar-toggle").click();
         }
     });
-		
-		$('.nav a').click(function(){
-			// close the menu once an item has been chosen, only if in mobile mode and open
-			if($("button.navbar-toggle").is(":visible") && $("#my-navbar li a#about").is(":visible")) {
-				setTimeout( function() {$('.navbar-collapse').collapse('hide');}, 600);
-			}
-		});
+    
+    $('.nav a').click(function(){
+      // close the menu once an item has been chosen, only if in mobile mode and open
+      if($("button.navbar-toggle").is(":visible") && $("#my-navbar li a#about").is(":visible")) {
+        setTimeout( function() {$('.navbar-collapse').collapse('hide');}, 600);
+      }
+    });
 
-		
-		// Initialize the document
-		
-		$('.active').toggleClass("active");         //turn off all active buttons
+    
+    // Initialize the document
+    
+    $('.active').toggleClass("active");         //turn off all active buttons
 
     //load content based on hash
     var id = window.location.hash;
@@ -107,5 +107,5 @@ $(document).ready(function(){
     else {
         $("#about").click();
     }
-		
+    
 });
